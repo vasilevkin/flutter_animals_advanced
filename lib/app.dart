bccf698ae19404773b5907a581d1ab515e76041a
Sergei Vasilevkin <svasilevkin@gmail.com>
2021-05-30 16:11:00 +0300

    Get all from simple cats and dogs implementation.

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animals_advanced/screens/home_screen.dart';
+
+class AnimalApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
